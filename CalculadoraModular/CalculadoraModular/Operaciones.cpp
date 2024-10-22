04903b15cc05817709f6867ab23b510477e80a83
@rafaelMartinez_ <rmartinez@visiotechsecurity.com>
2024-10-22 12:22:24 +0200

    [ADD] Se crean los proyectos de las secciones 1 y 2

---
+++ CalculadoraModular/CalculadoraModular/Operaciones.cpp
@@ -0,0 +1,26 @@
+#include "Operaciones.h"
+
+double sumar(double num1, double num2)
+{
+    return num1 + num2;
+}
+
+double restar(double num1, double num2)
+{
+    return num1 - num2;
+}
+
+double multiplicar(double num1, double num2)
+{
+    return num1 * num2;
+}
+
+double dividir(double num1, double num2)
+{
+    return num1 / num2;
+}
+
+int modulo(int num1, int num2)
+{
+    return num1 % num2;
+}
