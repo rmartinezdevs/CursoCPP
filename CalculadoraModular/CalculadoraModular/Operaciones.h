da62172c9f873b1fa71e5b0a0297ccf659e031c5
@rafaelMartinez_ <rmartinez@visiotechsecurity.com>
2024-11-18 12:12:49 +0100

    [REMOVE] Se reestructuran los proyectos del tema 2

--- CalculadoraModular/CalculadoraModular/Operaciones.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-double sumar(double num1, double num2);
-double restar(double num1, double num2);
-double multiplicar(double num1, double num2);
-double dividir(double num1, double num2);
-int modulo(int num1, int num2);
